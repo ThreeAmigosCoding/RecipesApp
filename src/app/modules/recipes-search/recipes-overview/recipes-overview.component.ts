e29c832da89b8fcc992a369b32cdaaf9e56a366f
lukaDjordjevic01 <96748944+lukaDjordjevic01@users.noreply.github.com>
2023-04-09 19:31:30 +0200

    Recipe cards overview and recipe search by title implemented

--- src/app/modules/recipes-search/recipes-overview/recipes-overview.component.ts
+++ src/app/modules/recipes-search/recipes-overview/recipes-overview.component.ts
@@ -31,6 +31,65 @@ export class RecipesOverviewComponent implements OnInit {
   selectedCuisines: Cuisine[] = [];
   selectedMealTypes: MealType[] = [];
 
+  recipes: Recipe[] = [
+    {
+      "title": "What to make for dinner tonight?? Bruschetta Style Pork & Pasta What to make for dinner tonight?? Bruschetta Style Pork & Pasta",
+      "preparationTime": 30,
+      "mealType": "Dinner",
+      "cuisine": "Italian",
+      "calories": 700
+    },
+    {
+      "title": "Veggie Stir Fry",
+      "preparationTime": 20,
+      "mealType": "Lunch",
+      "cuisine": "Asian",
+      "calories": 500
+    },
+    {
+      "title": "Beef Chili",
+      "preparationTime": 60,
+      "mealType": "Dinner",
+      "cuisine": "American",
+      "calories": 800
+    },
+    {
+      "title": "Pesto Pasta",
+      "preparationTime": 25,
+      "mealType": "Lunch",
+      "cuisine": "Italian",
+      "calories": 600
+    },
+    {
+      "title": "Fish Tacos",
+      "preparationTime": 35,
+      "mealType": "Dinner",
+      "cuisine": "Mexican",
+      "calories": 550
+    },
+    {
+      "title": "Beef Chili",
+      "preparationTime": 60,
+      "mealType": "Dinner",
+      "cuisine": "American",
+      "calories": 800
+    },
+    {
+      "title": "Pesto Pasta",
+      "preparationTime": 25,
+      "mealType": "Lunch",
+      "cuisine": "Italian",
+      "calories": 600
+    },
+    {
+      "title": "Fish Tacos",
+      "preparationTime": 35,
+      "mealType": "Dinner",
+      "cuisine": "Mexican",
+      "calories": 550
+    }
+  ]
+
   searchToken: string = "";
 
   chooseCuisine() {
@@ -40,4 +99,17 @@ export class RecipesOverviewComponent implements OnInit {
   chooseMealType() {
     this.mealTypesDialog.open(MealTypeSelectionComponent);
   }
+
+  titleContainsToken(title: string): boolean{
+    if (this.searchToken.trim() === "") return true;
+    return title.toLowerCase().includes(this.searchToken.trim().toLowerCase());
+  }
+}
+
+export interface Recipe {
+  title: string,
+  preparationTime: number,
+  mealType: string,
+  cuisine: string,
+  calories: number
 }
